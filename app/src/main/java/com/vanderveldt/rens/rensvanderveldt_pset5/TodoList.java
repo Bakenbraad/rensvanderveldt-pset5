a11f29ad973864fa8c601d3f4cb7bb2c525018cb
bakenbraad <rensvanderveldt@gmail.com>
2016-11-27 17:18:49 +0100

    Initial commit

---
+++ app/src/main/java/com/vanderveldt/rens/rensvanderveldt_pset5/TodoList.java
@@ -0,0 +1,34 @@
+package com.vanderveldt.rens.rensvanderveldt_pset5;
+
+/**
+ * Created by Rens on 27-11-2016.
+ */
+
+public class TodoList {
+
+    private String mastertitle;
+
+    public TodoList(String initMasterTitle){
+        setMastertitle(initMasterTitle);
+        // Add to sql database
+    }
+
+    public void setMastertitle(String setMasterTitle){
+        mastertitle = setMasterTitle;
+    }
+
+    public void addItem(String itemtitle, String itemdescription){
+        TodoItem newItem = new TodoItem(itemtitle, itemdescription);
+        newItem.setDescription(itemdescription);
+        newItem.setTitle(itemtitle);
+        // Add item to sql database
+    }
+
+    public void removeItem(long _id){
+        // remove item from sql database
+    }
+
+    public void removeList(long masterid){
+        
+    }
+}
---
+++ app/src/main/java/com/vanderveldt/rens/rensvanderveldt_pset5/TodoManager.java
@@ -0,0 +1,8 @@
+package com.vanderveldt.rens.rensvanderveldt_pset5;
+
+/**
+ * Created by Rens on 27-11-2016.
+ */
+
+public class TodoManager {
+}
